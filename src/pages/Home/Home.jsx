9f97fca585039feeb3708a8800fb099ae861b1c1
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-06 16:53:38 -0400

    Create Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,71 @@ import "./Home.css"
 
 export default function Home(){
     return(
-        <Header/>
+        <>
+            <Header/>
+
+            <div className="container">
+            <h2>Dashboard</h2>
+                <div className="cards">
+                    <div className="card">
+                        <p>Lorem, ipsum dolor.</p>
+                        <p className="result">2</p>
+                    </div>
+                    <div className="card">
+                        <p>Lorem, ipsum dolor.</p>
+                        <p className="result">40</p>
+                    </div>
+                    <div className="card">
+                        <p>Lorem, ipsum dolor.</p>
+                        <p className="result">2</p>
+                    </div>
+                    <div className="card">
+                        <p>Lorem, ipsum dolor.</p>
+                        <p className="result">1</p>
+                    </div>
+                </div>
+
+                <div className="tables">
+                    <div className="table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Itens Recentes</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>7 Worders</td>
+                                    <td><button>Ver</button></td>
+                                </tr>
+                                <tr>
+                                    <td>O Senhor dos Anéis</td>
+                                    <td><button>Ver</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Itens Acabando</th>
+                                    <th>Qtd.</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>7 Worders</td>
+                                    <td>8</td>
+                                    <td><button>Ver</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
