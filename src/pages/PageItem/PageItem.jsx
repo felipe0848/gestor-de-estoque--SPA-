23914eb9537ae136c427534f475178c2a107c267
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-11 10:24:24 -0400

    Include pageItem css and box-shadows

--- src/pages/PageItem/PageItem.jsx
+++ src/pages/PageItem/PageItem.jsx
@@ -1,17 +1,19 @@
 import Button from "../../Components/Button/Button";
 import useItemsCollection from "../../hooks/useItemsCollection";
+import "./PageItem.css"
+
 
 export default function PageItem() {
   const { removeItem, itemsInStock } = useItemsCollection();
 
   const id = "5hkfmfr2-4x3i-6cnf-z378-as0gzcsb7ey3";
-  const [{ name, qtd, price, category, description, createAt, updateAt }] = itemsInStock.filter(
-    (current) => current.id === id
-  );
-    const createDate = new Date (createAt)
-    const updateDate = new Date (updateAt)
+  const [{ name, qtd, price, category, description, createAt, updateAt }] =
+    itemsInStock.filter((current) => current.id === id);
+  const createDate = new Date(createAt);
+  const updateDate = new Date(updateAt);
+
   return (
-    <>
+    <div className="container">
       <div className="title">
         <h2>{name}</h2>
         <Button className="update" text="Atualizar" />
@@ -30,9 +32,19 @@ export default function PageItem() {
         <p>{description}</p>
       </div>
       <div className="dates">
-        <p>Cadastrado em: {createDate.toLocaleDateString('pt-br') + ' às ' + createDate.toLocaleTimeString('pt-br')}</p>
-        <p>Atualizado em: {updateDate.toLocaleDateString('pt-br') + ' às ' + updateDate.toLocaleTimeString('pt-br')}</p>
+        <p>
+          Cadastrado em:{" "}
+          {createDate.toLocaleDateString("pt-br") +
+            " às " +
+            createDate.toLocaleTimeString("pt-br")}
+        </p>
+        <p>
+          Atualizado em:{" "}
+          {updateDate.toLocaleDateString("pt-br") +
+            " às " +
+            updateDate.toLocaleTimeString("pt-br")}
+        </p>
       </div>
-    </>
+    </div>
   );
 }
